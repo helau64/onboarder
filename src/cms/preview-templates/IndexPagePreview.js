ef01ce135ed05b5b1b9973c1c8056738fa0905f1
Heather <hello@heatherlauren.me>
2019-05-31 12:11:41 +0100

    Remove unused home page fields

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -8,13 +8,7 @@ const IndexPagePreview = ({ entry, getAsset, widgetFor }) => {
   if (data) {
     return (
       <IndexPageTemplate
-        image={data.image}
         title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
         content={widgetFor('body')}
       />
     )
