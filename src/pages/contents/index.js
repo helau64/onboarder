d676e9f73bf59c75ef535b144e5819a788d2345a
Heather <hello@heatherlauren.me>
2019-06-13 11:52:03 +0100

    Change contents page to arrange content in JS

--- src/pages/contents/index.js
+++ src/pages/contents/index.js
@@ -1,58 +1,111 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
 
-const ContentsPage = ({
-  data: {
-    allMarkdownRemark: { group }
-  },
-}) => (
-  <Layout>
+export const ContentsPageTemplate = ({
+  sections,
+  pages
+}) => {
+
+  let groupedPages = []
+
+  sections.forEach(function(section) {
+    groupedPages.push(pages.filter(page => page.node.frontmatter.section.id === section.node.id))
+  })
+
+  return (
     <section className="contents-page">
         <h1 className="title">Contents</h1>
         <ul className="contents-list">
-            {group.map(section => (
-            <li key={section.fieldValue}>
-                <h2>{section.fieldValue}</h2>
-                <ul>
-                {section.edges.map(page => (
-                    <li key={page.node.id}>
-                        <Link to={page.node.fields.slug}>{page.node.frontmatter.title}</Link>
-                    </li>
-                ))}
-                </ul>
+          {sections.map((section, index) =>
+            <li>
+              <h2>{section.node.frontmatter.title}</h2>
+              <ul>
+                {groupedPages[index].map((page) =>
+                  <li>
+                    <Link to={page.node.fields.slug}>{page.node.frontmatter.title}</Link>
+                  </li>
+                )}
+              </ul>
+              
             </li>
-            ))}
+          )}
         </ul>
     </section>
-  </Layout>
-)
+  )
+}
+
+ContentsPageTemplate.propTypes = {
+  sections: PropTypes.array,
+  pages: PropTypes.array
+}
+
+const ContentsPage = ({ data }) => {
+  const { edges: pages } = data.pages
+  const { edges: sections } = data.sections
+
+  return (
+    <Layout>
+      <ContentsPageTemplate
+        sections={sections}
+        pages={pages}
+      />
+    </Layout>
+  )
+}
+
+ContentsPage.propTypes = {
+  data: PropTypes.shape({
+    pages: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+    sections: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  })
+}
 
 export default ContentsPage
 
 export const contentsPageQuery = graphql`
-  query ContentsPageQuery {
-    allMarkdownRemark(
-        filter: {frontmatter: {templateKey: {eq: "info-page"}}}
-        sort: {
-            fields: [frontmatter___order]
-            order: ASC
-        }
+  query ContentsPage {
+    pages: allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___order] }
+      filter: { frontmatter: { templateKey: { eq: "info-page" } } }
     ) {
-        group(field: frontmatter___section) {
-          fieldValue
-          edges {
-            node {
-              id
-              fields {
-                  slug
-              }
-              frontmatter {
-                title
-              }
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            section {
+                id
             }
           }
         }
       }
+    }
+    sections: allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___order] }
+      filter: { frontmatter: { templateKey: { eq: "section-page" } } }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+          }
+        }
+      }
+    }
   }
 `
